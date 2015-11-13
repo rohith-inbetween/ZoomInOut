0753bc68f6dad9cb87fd065debeefa7cd0e9fb92
Tauseef Ahmad <tauseef.ahmad@inbetween.co.in>
2015-11-13 21:07:58 +0530

    collapsed Template function written

--- function2.js
+++ function2.js
@@ -1,3 +1,5 @@
+var aVisualAttributes = [];
+
 $(document).ready(function(){
 
   var $VisAttrList = $('.visualAttributesListData');
@@ -24,6 +26,72 @@ $(document).ready(function(){
 
 });
 
+
+function generateUUID() {
+  var iCurrentTimeStamp = new Date().getTime();
+
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var iRandom = (iCurrentTimeStamp + Math.random() * 16) % 16 | 0;
+
+    iCurrentTimeStamp = Math.floor(iCurrentTimeStamp / 16);
+
+    return (c == 'x' ? iRandom : (iRandom & 0x3 | 0x8)).toString(16);
+  });
+
+  return uuid;
+}
+
+var _createVisualAttributeContainer = function(sTitle) {
+  var container = {
+    "id" : generateUUID(),
+    "type" : "container",
+    "title": sTitle ? sTitle : "Container",
+    "properties" : {
+      "sequence" : 0,
+      "style": {
+        "zIndex" : 0,
+        "height" : '150px',
+        "width" : '100%'
+      }
+    },
+    "contents" : []
+  };
+  return container;
+};
+
+var _createTextFrameVisualAttribute = function() {
+  var textFrame = {
+    "id" : generateUUID(),
+    "title" : "Text Frame",
+    "data": "",
+    "type" : "text-frame",
+    "properties" : {
+      style: {
+        'height': '150px'
+      }
+    },
+    "contents" : []
+  };
+  return textFrame;
+};
+
+var _createImageFrameVisualAttribute = function() {
+  var imageFrame = {
+    "id" : generateUUID(),
+    "title" : "Image Frame",
+    "type" : "image-frame",
+    "properties" : {
+      style: {
+        'height': '150px'
+      }
+    },
+    "data" : "",
+    "contents" : []
+  };
+  return imageFrame;
+};
+
+
 function setDropEvents($EditorDropZone){
   $EditorDropZone.on({
     drop: handleVisAttrDrop,
@@ -45,13 +113,41 @@ function handleVisAttrDrop(oEvent){
   } else if(oDroppedData.id == 'image-frame'){
     //oFrameData = createImageFrame();
   } else if(oDroppedData.id == 'container'){
-    //oFrameData = createContainer();
+    var oContent = _createVisualAttributeContainer();
+    oFrameData = createCollapsedTemplate(oContent);
   }
   if(oFrameData) {
-    aVisualAttributes.push(oFrameData.data);
-    $dropZone.before(oFrameData.uiElement);
-    $.event.trigger('resize',null,oFrameData.uiElement,false);
+    aVisualAttributes.push(oContent);
+    $dropZone.before(oFrameData);
+    //$.event.trigger('resize',null,oFrameData,false);
   }
   //$dropZone.parents('.visualAttributeContainer').trigger('resize');
 }
 
+function createCollapsedTemplate(oContent) {
+  var $template = $('<div class="templateContainer"></div>');
+
+  var $header = $('<div class="templateHeader">' +
+    '<div class="templateTitle">' + oContent.title + '</div>' +
+    '</div>' +
+  '</div>');
+
+  var $tileView = $('<div class="tileView"></div>');
+  if(oContent.type == 'text-frame') {
+    $tileView.append('<div class="tileData">' + oContent.data + '</div>');
+  } else if(oContent.type == 'image-frame'){
+    $tileView.append('<div class="tileData"><img class="tileImage" src=' + oContent.data + ' /></div>');
+
+  }
+  $header.append($tileView);
+
+  var $contentChildContainer = $('<div class="contentChildContainer"></div>');
+  _.forEach(oContent.contents, function (oVisualAttribute) {
+    $contentChildContainer.append(createCollapsedTemplate(oVisualAttribute));
+  });
+
+  $template.append($header);
+  $template.append($contentChildContainer);
+
+  return $template;
+}
