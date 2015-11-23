67b37455dc9db6ac919094127501d4554a404132
Vidit <vidit@inbetween.co.in>
2015-11-23 18:13:52 +0530

    CSS changes

--- react-ui/mockData.js
+++ react-ui/mockData.js
@@ -3,99 +3,36 @@ var uuid = require('./uuid');
 var oTemplates = {
   1: {
     "id": uuid.generateUUID(),
-    "type": "Motivation",
-    "title": "Motivation",
+    "type": "EZB Niedrigzins",
+    "title": "EZB Niedrigzins",
     "data": "",
     "contents": []
   },
   2: {
     "id": uuid.generateUUID(),
-    "type": "Proposal",
-    "title": "Proposal",
+    "type": "Vertrauen in DRV",
+    "title": "Vertrauen in DRV",
     "data": "",
     "contents": []
   },
   3: {
     "id": uuid.generateUUID(),
-    "type": "Allianz View",
-    "title": "Allianz View",
+    "type": "Versorgungslücke Immobilienbesitzer",
+    "title": "Versorgungslücke Immobilienbesitzer",
     "data": "",
     "contents": []
   },
   4: {
     "id": uuid.generateUUID(),
-    "type": "Market View",
-    "title": "Market View",
+    "type": "Renteninfo",
+    "title": "Renteninfo",
     "data": "",
     "contents": []
   },
   5: {
     "id": uuid.generateUUID(),
-    "type": "POS",
-    "title": "POS",
-    "data": "",
-    "contents": []
-  },
-  6: {
-    "id": uuid.generateUUID(),
-    "type": "PBS",
-    "title": "PBS",
-    "data": "",
-    "contents": []
-  },
-  7: {
-    "id": uuid.generateUUID(),
-    "type": "IB1",
-    "title": "IB1",
-    "data": "",
-    "contents": []
-  },
-  8: {
-    "id": uuid.generateUUID(),
-    "type": "IB2",
-    "title": "IB2",
-    "data": "",
-    "contents": []
-  },
-  9: {
-    "id": uuid.generateUUID(),
-    "type": "IB3",
-    "title": "IB3",
-    "data": "",
-    "contents": []
-  },
-  10: {
-    "id": uuid.generateUUID(),
-    "type": "IB4",
-    "title": "IB4",
-    "data": "",
-    "contents": []
-  },
-  11: {
-    "id": uuid.generateUUID(),
-    "type": "Headline",
-    "title": "Headline",
-    "data": "",
-    "contents": []
-  },
-  12: {
-    "id": uuid.generateUUID(),
-    "type": "Argument",
-    "title": "Argument",
-    "data": "",
-    "contents": []
-  },
-  13: {
-    "id": uuid.generateUUID(),
-    "type": "Summary",
-    "title": "Summary",
-    "data": "",
-    "contents": []
-  },
-  14: {
-    "id": uuid.generateUUID(),
-    "type": "Conclusion",
-    "title": "Conclusion",
+    "type": "Lücke schliessen",
+    "title": "Lücke schliessen",
     "data": "",
     "contents": []
   }
@@ -137,7 +74,7 @@ var editorData = [
                 "id": "1_1_1_1",
                 "type": "textFrame",
                 "title": "Intro",
-                "icon": "Import%20Filled-32White.png",
+                "icon": "About Filled-32.png",
                 "attributes": {
                   "briefing": ""
                 },
@@ -192,7 +129,7 @@ var editorData = [
                 "id": "1_1_1_2",
                 "type": "textFrame",
                 "title": "Details",
-                "icon": "View%20Details%20Filled-32White.png",
+                "icon": "Opera Glasses Filled-32.png",
                 "attributes": {
                   "briefing": ""
                 },
@@ -258,65 +195,10 @@ var editorData = [
                 "parentId": "1_1_1"
               },
               {
-                "id": "1_1_1_3",
-                "type": "textFrame",
-                "title": "Zusatzinfo",
-                "icon": "Info%20Filled-32.png",
-                "attributes": {
-                  "briefing": ""
-                },
-                "visibilityState": "expanded",
-                "data": "",
-                "contents": [
-                  {
-                    "id": "1_1_1_3_1",
-                    "type": "textFrame",
-                    "title": "Titel",
-                    "attributes": {
-                      "briefing": ""
-                    },
-                    "visibilityState": "expanded",
-                    "data": "",
-                    "contents": [
-
-                    ],
-                    "parentId": "1_1_1_3"
-                  },
-                  {
-                    "id": "1_1_1_3_2",
-                    "type": "HTML",
-                    "title": "Details",
-                    "attributes": {
-                      "briefing": ""
-                    },
-                    "visibilityState": "expanded",
-                    "data": "",
-                    "contents": [
-
-                    ],
-                    "parentId": "1_1_1_3"
-                  },{
-                    "id": "1_1_1_3_3",
-                    "type": "HTML",
-                    "title": "Fazit",
-                    "attributes": {
-                      "briefing": ""
-                    },
-                    "visibilityState": "expanded",
-                    "data": "",
-                    "contents": [
-
-                    ],
-                    "parentId": "1_1_1_3"
-                  }
-                ],
-                "parentId": "1_1_1"
-              },
-              {
                 "id": "1_1_1_4",
                 "type": "textFrame",
                 "title": "Verkaufschart",
-                "icon": "Bar%20Chart%20Filled-32White.png",
+                "icon": "Pie Chart-32.png",
                 "attributes": {
                   "briefing": ""
                 },
@@ -368,7 +250,7 @@ var editorData = [
                 "id": "1_1_1_5",
                 "type": "textFrame",
                 "title": "Spezialisteninfo",
-                "icon": "Worker-32Ehite.png",
+                "icon": "For Experienced Filled-32.png",
                 "attributes": {
                   "briefing": ""
                 },
